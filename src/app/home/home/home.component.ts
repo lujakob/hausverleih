04c99485b857002c1839e36019fe7e39baec4410
Lukas Jakob <lu.jakob@googlemail.com>
2018-02-10 17:19:55 +0100

    feat: Add home latest view

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {IIventoryItem} from "../../inventory/inventory.domain";
+import {Router} from "@angular/router";
+import {FirestoreService} from "../../shared/services/firestore.service";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +10,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public items: IIventoryItem[] = [];
+
+  displayedColumns = ['createdAt', 'title', 'category'];
+  dataSource = [];
+
+  constructor(
+    private firestoreService: FirestoreService,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.firestoreService
+      .colWithIds$('inventory', ref => ref.orderBy('createdAt', 'desc'))
+      .subscribe(data => this.dataSource = data);
   }
 
+  onClick(row) {
+    this.router.navigate(['inventory', row.id])
+  }
 }
